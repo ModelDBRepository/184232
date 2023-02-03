9421117a99d48d62c6c8b4db0118ce67d6a9f168
Rui Li <rui.li.rl895@yale.edu>
2023-02-03 13:34:42 -0500

    Updates for Python 3.x and recent NEURON (#1)

--- neuron472434498.py
+++ neuron472434498.py
@@ -14,7 +14,8 @@ class Neuron472434498:
             
         Note: if name is not specified, Neuron472434498_instance is used instead
         '''
-        
+                
+        self._name = name
         # load the morphology
         from load_swc import load_swc
         load_swc('Rorb-IRES2-Cre-D_Ai14_IVSCC_-172651.01.01.01_464113513_m.swc', self,
@@ -31,8 +32,7 @@ class Neuron472434498:
         self.axon[0].connect(self.soma[0](0.5))
         self.axon[1].connect(self.axon[0](1))
         self.all += self.axon
-        
-        self._name = name
+
         self._insert_mechanisms()
         self._discretize_model()
         self._set_mechanism_parameters()
